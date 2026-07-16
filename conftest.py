88d71332df455b506833ce027ca1983d133f8c90
powero4ek <ilaockin0122@gmail.com>
2026-07-16 20:59:38 +0300

    fix conftest

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import pytest
@@ -6,7 +7,18 @@ import pytest
 
 @pytest.fixture
 def browser():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    options = Options()
+
+    options.add_argument("--headless=new")      # запуск без окна
+    options.add_argument("--no-sandbox")        # обязательно для Docker
+    options.add_argument("--disable-dev-shm-usage")  # обязательно для Docker
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
 
     yield driver
 
