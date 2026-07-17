442dd8bedb599cc05613f366421182160dd4ca97
powero4ek <124286600+powero4ek@users.noreply.github.com>
2026-07-17 18:19:17 +0300

    Update test_yandex.py

--- test_yandex.py
+++ test_yandex.py
@@ -1,5 +1,3 @@
-from YandexPages import SearchHelper
-
 def test_yandex_search(browser):
 
     yandex_main_page = SearchHelper(browser)
@@ -8,8 +6,6 @@ def test_yandex_search(browser):
 
     yandex_main_page.enter_word("Hello")
 
-    yandex_main_page.wait_url_contains("search")
+    print(browser.current_url)
 
-    url = yandex_main_page.get_current_url()
-
-    assert "search" in url
+    assert False
